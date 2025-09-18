155b5ad25e835a1eab68826424b8965f01f4cdce
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-18 20:42:49 +0700

    Add files via upload

--- db.js
+++ db.js
@@ -1,3 +1,12 @@
 {
-  "users": {}
+  "users": {
+    "admin": {
+      "password": "admin123",
+      "createdAt": "2025-01-01T00:00:00.000Z"
+    },
+    "user1": {
+      "password": "password1",
+      "createdAt": "2025-01-02T00:00:00.000Z"
+    }
+  }
 }
